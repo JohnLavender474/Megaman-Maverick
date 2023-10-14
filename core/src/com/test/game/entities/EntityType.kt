17e2cfd5d171b415cc8c98180c7ec1efee7f9e3f
John Lavender <82977289+JohnLavender474@users.noreply.github.com>
2023-10-14 17:10:08 -0400

    Working on factories

---
+++ core/src/com/test/game/entities/EntityType.kt
@@ -0,0 +1,17 @@
+package com.test.game.entities
+
+enum class EntityType {
+    BLOCK,
+    DECORATION,
+    EXPLOSION,
+    ENEMY,
+    BOSS,
+    HAZARD,
+    ITEM,
+    TEST,
+    INTERACTIVE,
+    MEGAMAN,
+    PROJECTILE,
+    SENSOR,
+    SPECIAL
+}
--- core/src/com/test/game/entities/factories/BlockFactory.kt
+++ core/src/com/test/game/entities/factories/BlockFactory.kt
@@ -1,4 +1,33 @@
 package com.test.game.entities.factories
 
-class BlockFactory {
+import com.badlogic.gdx.utils.ObjectMap
+import com.engine.IGame2D
+import com.engine.common.objects.Pool
+import com.engine.entities.IGameEntity
+import com.engine.factories.IFactory
+import com.test.game.entities.blocks.Block
+
+class BlockFactory(private val game: IGame2D) : IFactory<IGameEntity> {
+
+  companion object {
+    const val STANDARD = "Standard"
+    const val ICE_BLOCK = "IceBlock"
+    const val GEAR_TROLLEY = "GearTrolley"
+    const val CONVEYOR_BELT = "ConveyorBelt"
+    const val ROCKET_PLATFORM = "RocketPlatform"
+  }
+
+  private val pools = ObjectMap<Any, Pool<Block>>()
+
+  init {
+    pools.put(
+        STANDARD,
+        EntityPoolCreator.create(
+            10,
+        ) {
+          Block(game)
+        })
+  }
+
+  override fun fetch(key: Any): Block? = pools.get(key)?.fetch()
 }
