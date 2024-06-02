61cb7132c76962e968a1a1a5be5fa4102c184506
JohnLavender474 <johnmichaellavender123@gmail.com>
2024-06-02 15:30:03 -0500

    Change default SELECT key to L

--- core/src/com/megaman/maverick/game/controllers/ControllerButton.kt
+++ core/src/com/megaman/maverick/game/controllers/ControllerButton.kt
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Input
 
 enum class ControllerButton(val defaultKeyboardKey: Int) {
     START(Input.Keys.ENTER),
-    SELECT(Input.Keys.L),
+    SELECT(Input.Keys.I),
     UP(Input.Keys.W),
     DOWN(Input.Keys.S),
     LEFT(Input.Keys.A),
