1608358d5a42c345be92e3de84c72d9a660913b4
JohnLavender474 <johnmichaellavender123@gmail.com>
2024-07-15 00:18:57 -0400

    Begin on SkyMan stage

---
+++ assets/tiled_maps/tsx/SkyMan.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="SkyMan" tilewidth="32" tileheight="32" tilecount="17280" columns="192">
+ <image source="../png/SkyMan.png" width="6144" height="2880"/>
+</tileset>
