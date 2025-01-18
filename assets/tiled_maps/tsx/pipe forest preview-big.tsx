869e05836354a6c857da65ec713cee768d47a5a2
JohnLavender474 <johnmichaellavender123@gmail.com>
2025-01-18 14:38:25 -0500

    New images for Axe Joe and Timber Woman's stage

---
+++ assets/tiled_maps/tsx/pipe forest preview-big.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="pipe forest preview-big" tilewidth="32" tileheight="32" tilecount="1452" columns="66">
+ <image source="../png/pipe forest preview-big.png" width="2112" height="720"/>
+</tileset>
---
+++ assets/tiled_maps/tsx/pipe forest preview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="pipe forest preview" tilewidth="32" tileheight="32" tilecount="660" columns="44">
+ <image source="../png/pipe forest preview.png" width="1408" height="480"/>
+</tileset>
