181d87c2c37e5c130adb4029522b4a34152f98de
John Lavender <johnmichaellavender123@gmail.com>
2026-05-02 12:00:40 -0400

    Fix issues with credits screen, fade out on START/SELECT button press

--- core/src/main/kotlin/com/megaman/maverick/game/screens/other/CreditsScreen.kt
+++ core/src/main/kotlin/com/megaman/maverick/game/screens/other/CreditsScreen.kt
@@ -1,20 +1,27 @@
 package com.megaman.maverick.game.screens.other
 
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver
+import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.utils.Array
 import com.badlogic.gdx.utils.ObjectMap
 import com.badlogic.gdx.utils.Queue
 import com.mega.game.engine.common.GameLogger
+import com.mega.game.engine.common.extensions.gdxArrayOf
+import com.mega.game.engine.common.extensions.getTextureRegion
 import com.mega.game.engine.common.time.Timer
 import com.mega.game.engine.screens.BaseScreen
+import com.megaman.maverick.game.ConstKeys
 import com.megaman.maverick.game.ConstVals
 import com.megaman.maverick.game.MegamanMaverickGame
 import com.megaman.maverick.game.assets.MusicAsset
+import com.megaman.maverick.game.assets.SoundAsset
+import com.megaman.maverick.game.assets.TextureAsset
+import com.megaman.maverick.game.controllers.MegaControllerButton
 import com.megaman.maverick.game.drawables.fonts.MegaFontHandle
 import com.megaman.maverick.game.screens.ScreenEnum
+import com.megaman.maverick.game.screens.utils.Fade
+import com.megaman.maverick.game.screens.utils.Fade.FadeType
 import com.megaman.maverick.game.utils.extensions.setToDefaultPosition
 
 object CreditsLoader {
@@ -40,11 +47,18 @@ class CreditsScreen(
         private const val DELAY_TIME = 1f
         private const val SPEED = 2.5f
         private const val LERP_ALPHA = 10f
-        private const val FADE_OUT_TIME = 2f
+        private const val FADE_OUT_TIME = 1f
+        private const val FULL_END_TIME = 1.5f
+        private val EARLY_EXIT_BUTTONS = gdxArrayOf<Any>(
+            MegaControllerButton.START,
+            MegaControllerButton.SELECT
+        )
     }
 
     private val delayTimer = Timer(DELAY_TIME)
-    private val fadeOutTimer = Timer(FADE_OUT_TIME)
+    private val fullEndTimer = Timer(FULL_END_TIME)
+
+    private val fadeOut = Fade(FadeType.FADE_OUT, FADE_OUT_TIME)
 
     private val activeFonts = Array<MegaFontHandle>()
     private val fontTargets = ObjectMap<MegaFontHandle, Float>()
@@ -54,21 +68,45 @@ class CreditsScreen(
 
     override fun show() {
         GameLogger.debug(TAG, "show()")
-        delayTimer.reset()
+
         creditsQueue = CreditsLoader.load()
+
         game.getUiCamera().setToDefaultPosition()
         game.audioMan.playMusic(MusicAsset.VINNYZ_WIP_1_MUSIC, true)
+
+        val blackRegion = game.assMan.getTextureRegion(
+            TextureAsset.COLORS.source,
+            ConstKeys.BLACK
+        )
+        fadeOut.setRegion(blackRegion)
+        fadeOut.setPosition(0f, 0f)
+        fadeOut.setWidth(ConstVals.VIEW_WIDTH * ConstVals.PPM)
+        fadeOut.setHeight(ConstVals.VIEW_HEIGHT * ConstVals.PPM)
+        fadeOut.init()
+    }
+
+    private fun shouldSkipCredits() =
+        game.controllerPoller.isAnyJustPressed(EARLY_EXIT_BUTTONS)
+
+    private fun setCreditsComplete() {
+        GameLogger.debug(TAG, "setCreditsComplete()")
+        creditsComplete = true
+        game.audioMan.fadeOutMusic(FADE_OUT_TIME)
     }
 
     override fun render(delta: Float) {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            if (game.paused) game.pause()
-            else game.resume()
+        if (!creditsComplete && shouldSkipCredits()) {
+            GameLogger.debug(TAG, "render(): player triggered early exit")
+            game.audioMan.playSound(SoundAsset.SELECT_PING_SOUND, false)
+            setCreditsComplete()
         }
-        if (game.paused) return
 
         if (creditsComplete) {
-            handleFadeout(delta)
+            fadeOut.update(delta)
+
+            fullEndTimer.update(delta)
+            if (fullEndTimer.isFinished()) onCompletion.invoke()
+
             return
         }
 
@@ -79,11 +117,14 @@ class CreditsScreen(
         }
 
         updateCreditsPosition(delta)
+    }
 
-        game.batch.projectionMatrix = game.getUiCamera().combined
-        game.batch.begin()
-        activeFonts.forEach { it.draw(game.batch) }
-        game.batch.end()
+    override fun draw(drawer: Batch) {
+        drawer.projectionMatrix = game.getUiCamera().combined
+        drawer.begin()
+        activeFonts.forEach { it.draw(drawer) }
+        if (creditsComplete) fadeOut.draw(drawer)
+        drawer.end()
     }
 
     private fun updateCreditsPosition(delta: Float) {
@@ -101,11 +142,23 @@ class CreditsScreen(
                 fontTargets.remove(font)
             }
         }
-        if (creditsQueue.isEmpty && activeFonts.isEmpty) {
-            creditsComplete = true
-            fadeOutTimer.reset()
-            game.audioMan.fadeOutMusic(FADE_OUT_TIME)
+        if (!creditsComplete && creditsQueue.isEmpty && activeFonts.isEmpty) {
+            GameLogger.debug(TAG, "updateCreditsPosition(): credits queue empty")
+            setCreditsComplete()
         }
+    }
+
+    override fun reset() {
+        GameLogger.debug(TAG, "reset()")
+        creditsComplete = false
+
+        fadeOut.reset()
+
+        delayTimer.reset()
+        fullEndTimer.reset()
+
+        activeFonts.clear()
+        fontTargets.clear()
     }
 
     private fun spawnNextFont() {
@@ -113,10 +166,5 @@ class CreditsScreen(
         nextFont.positionY = 0f
         fontTargets.put(nextFont, 0f)
         activeFonts.add(nextFont)
-    }
-
-    private fun handleFadeout(delta: Float) {
-        fadeOutTimer.update(delta)
-        if (fadeOutTimer.isFinished()) onCompletion.invoke()
     }
 }
