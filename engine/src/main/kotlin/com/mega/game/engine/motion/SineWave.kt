e2c56911240f2fae5b79a6390b4cef2edef8048b
John Lavender <johnmichaellavender123@gmail.com>
2026-05-20 21:53:20 -0400

    Update sine utils to make position always relative to the origin

--- engine/src/main/kotlin/com/mega/game/engine/motion/SineWave.kt
+++ engine/src/main/kotlin/com/mega/game/engine/motion/SineWave.kt
@@ -6,16 +6,18 @@ import com.mega.game.engine.common.utils.SineUtils
 class SineWave(var position: Vector2, var speed: Float, var amplitude: Float, var frequency: Float) : IMotion {
 
     private var elapsedTime = 0f
+    private val origin = Vector2(position)
 
     override fun getMotionValue(out: Vector2): Vector2 = out.set(position)
 
     override fun update(delta: Float) {
         elapsedTime += delta
-        position.x += speed * delta
-        position.y += SineUtils.y(amplitude, frequency, elapsedTime)
+        position.x = origin.x + speed * elapsedTime
+        position.y = origin.y + SineUtils.y(amplitude, frequency, elapsedTime)
     }
 
     override fun reset() {
         elapsedTime = 0f
+        position.set(origin)
     }
 }
