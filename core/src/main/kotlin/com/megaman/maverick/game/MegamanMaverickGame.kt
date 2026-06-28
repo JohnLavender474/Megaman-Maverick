6723b1b31acd3891ccdca2e8c9df88be1e36a7e2
John Lavender <johnmichaellavender123@gmail.com>
2026-06-28 11:41:54 -0400

    Bump game version up to 1.14.4

--- core/src/main/kotlin/com/megaman/maverick/game/MegamanMaverickGame.kt
+++ core/src/main/kotlin/com/megaman/maverick/game/MegamanMaverickGame.kt
@@ -150,7 +150,7 @@ class MegamanMaverickGame(
 
     companion object {
         const val TAG = "MegamanMaverickGame"
-        const val VERSION = "ALPHA 1.14.3"
+        const val VERSION = "ALPHA 1.14.4"
         private const val ASSET_MILLIS = 17
         private const val LOADING = "LOADING"
         private const val LOG_FILE_NAME = "logs.txt"
